6b88ede0a67c769a0c8157f82cb25c8ab2a0cdf2
Lais Schiavone Sobral <a202304133441@alunos.estacio.br>
2023-09-23 13:09:54 -0300

    Completo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numeroDigitado,centenas,dezenas,unidade;  
+Console.Write("Digite um número inteiro...:"); 
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"O número digitado é {numeroDigitado}");
+
+centenas=numeroDigitado / 100;
+dezenas= (numeroDigitado) % 100 / 10;
+unidade= (numeroDigitado) % 100 % 10;
+Console.WriteLine($"O numero possui {centenas} centena (s)");
+Console.WriteLine($"{dezenas,17} dezena (s)");
+Console.Write($"{unidade,17} unidade (s)");
